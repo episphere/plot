0dc4af3239f924a02f58419048b192f8a7ca6439
Jonas Almeida <jalmeida@mathbiol.org>
2020-08-23 11:55:14 -0400

    load import json

--- plot.js
+++ plot.js
@@ -1,28 +1,55 @@
-console.log('census.js loaded')
+console.log('plot.js loaded')
 
-census={}
+plot={}
 
-census.get=async(q='/data/',apiURL='https://us-central1-nih-nci-dceg-episphere-dev.cloudfunctions.net/episphere_census')=>{ // default returns catalog, could also be, for example census.get('/data/2018/acs/acs1/subject?get=NAME,group(S0101)&for=us:1')
-    let url=`${apiURL}?${encodeURIComponent(q)}`
-    if(!q.match(/\?/g)){
-        url=`${apiURL}?${encodeURIComponent(q)}?date=${Date()}`
+plot.draw=async(obj,fname,div='plotDiv')=>{
+    console.log(`drawing ${obj}`)
+    if(typeof(obj)=='string'){
+        obj = await(await fetch(obj)).json()
+        console.log(`${obj} loaded`)
     }
-    //console.log(`calling ${url}`)
-    let res = await (await fetch(url)).text()
+    if(typeof(div)=='string'){
+        div=document.getElementById(div)
+        console.log('looking for', div)
+    }
+    if(typeof(div)=='undefined'){
+        div=document.createElement('div')
+        div.id=div
+        console.log(div,'created')
+    }
+    // clear div
+    div.innerHTML=''
+    div.className=''
+    // draw figure in child div
+    let divPlotly = document.createElement('div')
+    div.appendChild(divPlotly)
+    Plotly.plot(divPlotly,obj.traces,obj.layout)
+    // export traces as csv
+    let divTools = document.createElement('div')
+    div.appendChild(divTools)
+    divTools.innerHTML=`<a href="#" onclick="plot.extractTraces(obj.traces,'${fname}')">${fname}</a>`
+
     //debugger
-    try {
-        res=JSON.parse(res)
-    }
-catch(err) {
-    console.log(res)
-    }
-    return res
+}
+plot.extractTraces=(traces,fname)=>{
+    debugger
 }
 
-// example
-// census.get('/data/2018/acs/acs1/subject?get=NAME,group(S0101)&for=us:1')
-// for debugging with local server: census.get=async(q='/data/',apiURL='http://localhost:3000')
+plot.saveFile=function(x,fileName) { // x is the content of the file
+	var bb = new Blob([x]);
+   	var url = URL.createObjectURL(bb);
+	var a = document.createElement('a');
+   	a.href=url;
+	if (fileName){
+		if(typeof(fileName)=="string"){ // otherwise this is just a boolean toggle or something of the sort
+			a.download=fileName;
+		}
+		a.click() // then download it automatically 
+	} 
+	return a
+}
+
 
 if(typeof(define)!='undefined'){
-    define(census)
+    define(plot)
 }
